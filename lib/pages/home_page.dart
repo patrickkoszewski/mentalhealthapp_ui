c0a9f7dfd717a3a85d1436fd0a94b6af03fc6fc8
Patrick <ledrap1992@gmail.com>
2022-12-08 18:25:22 +0100

    Add Emotiocn how do you feel

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mentalhealthapp_ui/util/emoticon_face.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -12,54 +13,233 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.blue[900],
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: ''),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: ''),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: ''),
+        ],
+      ),
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(25.0),
-          child: Column(
-            children: [
-              //greetings row
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Column(
                 children: [
-                  //Hi Jared!
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+                  //greetings row
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      const Text(
-                        'Hi, Jared!',
-                        style: TextStyle(
+                      //Hi Jared!
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            'Hi, Jared!',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 24,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          const SizedBox(
+                            height: 8,
+                          ),
+                          Text(
+                            '23 Dec, 2022',
+                            style: TextStyle(color: Colors.blue[200]),
+                          ),
+                        ],
+                      ),
+
+                      //Notification
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.blue[700],
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        child: const Padding(
+                          padding: EdgeInsets.all(12.0),
+                          child: Icon(
+                            Icons.notifications,
                             color: Colors.white,
-                            fontSize: 24,
-                            fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  //search bar
+                  Container(
+                    padding: const EdgeInsets.all(12),
+                    decoration: BoxDecoration(
+                        color: Colors.blue[700],
+                        borderRadius: BorderRadius.circular(12)),
+                    child: Row(children: const [
+                      Icon(
+                        Icons.search,
+                        color: Colors.white,
                       ),
                       SizedBox(
-                        height: 8,
+                        width: 8,
                       ),
                       Text(
-                        '23 Dec, 2022',
-                        style: TextStyle(color: Colors.blue[200]),
+                        'Search',
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ]),
+                  ),
+
+                  const SizedBox(
+                    height: 25,
+                  ),
+
+                  //how do you feel?
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: const [
+                      Text(
+                        'How do you feel?',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      Icon(
+                        Icons.more_horiz,
+                        color: Colors.white,
                       ),
                     ],
                   ),
 
-                  //Notification
-                  Container(
-                    decoration: BoxDecoration(
-                      color: Colors.blue[700],
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                    child: const Padding(
-                      padding: EdgeInsets.all(12.0),
-                      child: Icon(
-                        Icons.notifications,
-                        color: Colors.white,
+                  const SizedBox(
+                    height: 25,
+                  ),
+
+                  //4 diffrent faces
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      //bad
+                      Column(
+                        children: const [
+                          EmoticonFace(
+                            emoticonFace: '😥',
+                          ),
+                          SizedBox(
+                            height: 8,
+                          ),
+                          Text(
+                            'Bad',
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          )
+                        ],
                       ),
-                    ),
-                  )
+
+                      //fine
+                      Column(
+                        children: const [
+                          EmoticonFace(
+                            emoticonFace: '☺',
+                          ),
+                          SizedBox(
+                            height: 8,
+                          ),
+                          Text(
+                            'Fine',
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          )
+                        ],
+                      ),
+                      //well
+                      Column(
+                        children: const [
+                          EmoticonFace(
+                            emoticonFace: '😁',
+                          ),
+                          SizedBox(
+                            height: 8,
+                          ),
+                          Text(
+                            'Well',
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          )
+                        ],
+                      ),
+                      //excellent
+                      Column(
+                        children: const [
+                          EmoticonFace(
+                            emoticonFace: '🥳',
+                          ),
+                          SizedBox(
+                            height: 8,
+                          ),
+                          Text(
+                            'Excellent',
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
                 ],
               ),
-            ],
-          ),
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            Expanded(
+              child: Container(
+                padding: const EdgeInsets.all(25),
+                color: Colors.white,
+                child: Center(
+                  child: Column(
+                    children: [
+                      //exercise heading
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: const [
+                          Text(
+                            'Exercises',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 20),
+                          ),
+                          Icon(Icons.more_horiz),
+                        ],
+                      ),
+                      //listView of Exercises
+                      //gonna repeat a lot of things so create new file
+                      Container(
+                        decoration: BoxDecoration(color: Colors.grey.shade300),
+                        child: const ListTile(
+                          leading: Icon(
+                            Icons.favorite,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
